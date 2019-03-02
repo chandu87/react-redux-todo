25ea5221c8f20492796e43e8200780dba9cf094a
chandu <chandrasr.palle@gmail.com>
2019-03-02 11:10:38 +0100

    commit with updated boilerplate

--- app/tests/app.test.jsx
+++
@@ -1,8 +0,0 @@
-var expect= require('expect');
-
-describe('App',()=>{
-  it('it should run properly', ()=>{
-    expect(1).toBe(1);
-  });
-  
-});
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodoApp extends React.Component{
+    render(){
+        return("hi");
+    }
+}
+
+export default TodoApp;
